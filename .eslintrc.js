683fe50a71cf7f93ff570161e00e83dcf52c1c5f
liukaixin <15713857919@163.com>
2024-04-01 15:26:40 +0800

    feat: react

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // env：脚本将要运行在什么环境中
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
+};
